3cf7480c8ebda37f46eb5adb9236a2fca633db39
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2023-07-16 02:25:39 +0600

    15 July's Task

---
+++ 6_1 Fibonacci using Recursion.cpp
@@ -0,0 +1,22 @@
+// Problem: Calculate the nth Fibonacci Number
+// 0, 1, 1, 2, 3, 5, 8, 13
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if(n==0) return 0;
+    if(n==1) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main ()
+{
+    int n;
+    cin >> n;
+    cout << fibonacci(n) <<"\n";
+    return 0;
+}
