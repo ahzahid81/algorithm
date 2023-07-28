ea1ba1c1f6d8cc938c02f5b066275e6e9dd0e743
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2023-07-28 06:19:42 +0600

    28 July's Task

---
+++ 21_3 Tabulation and Memorization.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 101;
+
+int dp[N]; // dp array
+
+int fib(int n)
+{
+    if(n <=2) return 1;
+
+    if(dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    int ans = fib(n-1) + fib(n-2);
+    dp[n] = ans;
+    return ans;
+}
+
+int main ()
+{
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+    {
+        dp[i] = -1;
+    }
+cout << fib(n) <<"\n";
+
+}
